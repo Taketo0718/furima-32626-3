db92e8da9991fd657ca347d36b8b2b6d4492248d
Taketo0718 <68280188+gozin56@users.noreply.github.com>
2020-12-11 12:28:51 +0900

    userモデルバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :birth_date, presence: true
-
+  
+  validates :password, format:
+  { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers" }
+  
   with_options presence: true, format:
-   { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+  { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: "Full-width characters" } do
     validates :last_name
     validates :first_name
   end
-
+  
   with_options presence: true, format:
-   { with: /\A[ァ-ヶー－]+\z/, message: 'に全角文字を使用してください' } do
+  { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" } do
     validates :last_name_kana
     validates :first_name_kana
   end
+  
+  validates :birth_date, presence: true
 
-  validates :password, format:
-   { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
 end
