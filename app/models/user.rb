841b5933b682075c99e3bbd51ab1df3b971c8f51
Taketo0718 <68280188+gozin56@users.noreply.github.com>
2020-12-11 12:10:22 +0900

    ログイン条件分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_date, presence: true
+
+  with_options presence: true, format:
+   { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format:
+   { with: /\A[ァ-ヶー－]+\z/, message: 'に全角文字を使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :password, format:
+   { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
 end
