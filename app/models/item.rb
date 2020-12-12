83a8071f06265868e3989fbda5568ffd2c7ee373
Taketo0718 <68280188+gozin56@users.noreply.github.com>
2020-12-13 01:08:51 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,26 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_prefecture
+  belongs_to :item_sales_status
+  belongs_to :item_scheduled_delivery
+  belongs_to :item_shipping_fee_status
+
   with_options presence: true do
     validates :image
     validates :item_name
     validates :item_info
-    validates :item_category
-    validates :item_sales_status
-    validates :item_shipping_fee_status
-    validates :item_prefecture
-    validates :item_scheduled_delivery
     validates :item_price
   end
+
+  with_options numericality: { other_than: 1 }  do
+    validates :item_category_id
+    validates :item_sales_status_id
+    validates :item_shipping_fee_status_id
+    validates :item_prefecture_id
+    validates :item_scheduled_delivery_id
+  end
+
   belongs_to :user
   has_one_attached :image
 end
--- db/migrate/20201212124418_create_items.rb
+++ db/migrate/20201212124418_create_items.rb
@@ -3,11 +3,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :item_name
       t.text :item_info
-      t.integer :item_category
-      t.integer :item_sales_status
-      t.integer :item_shipping_fee_status
-      t.integer :item_prefecture
-      t.integer :item_scheduled_delivery
+      t.integer :item_category_id
+      t.integer :item_sales_status_id
+      t.integer :item_shipping_fee_status_id
+      t.integer :item_prefecture_id
+      t.integer :item_scheduled_delivery_id
       t.integer :item_price
       t.references :user, foreign_key: true
       t.timestamps
