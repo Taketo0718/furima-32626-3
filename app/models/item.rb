5a4ed93346e7a95e82f4f8aef40c72cd07677fa5
Taketo0718 <68280188+gozin56@users.noreply.github.com>
2020-12-12 22:17:11 +0900

    itemモデルバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :item_name
+    validates :item_info
+    validates :item_category
+    validates :item_sales_status
+    validates :item_shipping_fee_status
+    validates :item_prefecture
+    validates :item_scheduled_delivery
+    validates :item_price
+  end
   belongs_to :user
 end
