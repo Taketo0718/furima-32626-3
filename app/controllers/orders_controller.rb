431d9b06153f5e27dbad1ecfd8e31786402ffd6c
Taketo0718 <68280188+gozin56@users.noreply.github.com>
2020-12-19 19:42:58 +0900

    購入機能実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,49 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, except:[:new, :item_order_params]
+  before_action :move_to_index
+
+  def index
+    @item_order=ItemOrder.new
+  end
+
+  def new
+  end
+
+  def create
+    @item_order = ItemOrder.new(item_order_params)
+    if @item_order.valid?
+      pay_item
+      @item_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def item_order_params
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+                               .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.item_price,
+      card: item_order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if current_user == @item.user
+      redirect_to root_path
+    end
+  end
+end
---
+++ app/models/item_order.rb
@@ -0,0 +1,22 @@
+class ItemOrder
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :item_id, :user_id, :token
+
+  with_options presence: true do
+    validates :token
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
+    validates :city
+    validates :addresses
+    validates :phone_number, numericality: { only_integer: true, message: "Input only number" }
+    validates :item_id
+    validates :user_id
+  end
+
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
+    
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city,
+                   addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
